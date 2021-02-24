443cf120d4e5eb6f4c94052b701844e0baaffdd0
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-02-23 21:37:41 -0300

    hfix: Fixed a null pointer in Matrix, feat: Added more commentaries to the code

--- src/utils/matrix.cpp
+++ src/utils/matrix.cpp
@@ -6,6 +6,7 @@ class Matrix {
 
     private: 
 
+    //Nested class Cell, to store an element within each point in the array
     class Cell {
 
         private:
@@ -24,7 +25,6 @@ class Matrix {
             this->right   = NULL;
             this->up      = NULL;
             this->down    = NULL;
-            this->element = nullptr;
         }
 
         T getElement() {
@@ -39,25 +39,47 @@ class Matrix {
 
     public: 
 
+    //Constructor 1: no size specified, creating 3x3 matrix
     Matrix() {
         this->number_of_columns = 3;
         this->number_of_rows    = 3;
         buildMatrix();
     }
 
+    //Constructor 2: creating a NxN matrix
     Matrix(short n) {
-        this->number_of_columns = this->number_of_rows = n;
+        if(n < 3) {
+            n = 3;
+        }
+        else {
+            this->number_of_columns = this->number_of_rows = n;
+        }
+
         buildMatrix();
     }
 
+    //Constructor 3: creating a CxR matrix
     Matrix(short c, short r) {
-        this->number_of_columns = c;
-        this->number_of_rows    = r;
+
+        if( c < 3 || r < 3 ) {
+            if( c < 3) {
+                c = 3;
+            }
+            if ( r < 3) {
+                r = 3;
+            }
+        }
+        else {
+            this->number_of_columns = c;
+            this->number_of_rows    = r;
+        }
+
         buildMatrix();
     }
 
     private: 
 
+    //Creating linked cells to the first cell of the Matrix
     void buildMatrix() {
 
         //Creating the first element
@@ -78,32 +100,40 @@ class Matrix {
             tmp = tmp->right;
         }
 
-        //reset the pointer to the first cell
+        //reset the pointer to link to the first cell
         tmp = this->first;
 
         //Deploy consecutive rows of cells in the Matrix
         for (int i = 0; i < c;i++) {
 
-            //Start the first element
+            //Start the first element 
             Cell* tmp2 = new Cell();
             tmp->down = tmp2;
             tmp2->up  = tmp;
 
             for(int j = 0; j < r; j++) {
-                Cell* tmp3 = new Cell();
+                Cell* tmp3  = new Cell();
+
+                //Linking cells horizontally
                 tmp2->right = tmp3;
-                tmp3->left = tmp2;
+                tmp3->left  = tmp2;
+
+                //Linking cells vertically
                 tmp2->up->right->down = tmp3;
                 tmp3->up = tmp2->up->right;
 
+                //Moving the pointer to the right, to continue to create the row
                 tmp2 = tmp2->right;
             }
+
+            //After creating a row, go to the next one
             tmp = tmp->down;
         }
     }
 
     public:
 
+    //Print the Matrix using std::cout
     void print() {
         for(Cell* i = this->first; i != NULL ; i = i->down ) {
             for(Cell* j = i; j != NULL ; j = j->right ) 
@@ -111,5 +141,7 @@ class Matrix {
             
             std::cout << "\n" ; //Add spacing in between the columns
         }
-     }
+    }
+
+
 };
