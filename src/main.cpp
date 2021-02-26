d90670e6cc28f779a349293979e6d2048d53c1d3
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-02-26 15:35:43 -0300

    feat: added maps with list of strings, feat: added a simple choice system in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,44 @@
 #include<iostream>
+#include<map>
 #include<string>
-#include "utils/matrix.cpp"
+#include<list>
 
+#ifdef linux
+#define clear() std::system("clear")
+#else
+#define clear() system("CLS")
+#endif
+
+void menu();
+void printMenu(std::list<std::string> texts);
+
+std::map<int,std::list<std::string>> menu_text{
+    {0,{"1 - Unweighted targeted graph","2 - Unweighted untargeted graph","3 - Weighted untargeted graph","4 - Weighted targeted graph","0 - exit"}}
+};
 int main() {
-    /*
-    Matrix<std::string>* m = new Matrix<std::string>();
-    m->print();
-    m->insert("teste",0,1);
-    m->print();
-    std::cout << m->get(0,1) << std::endl;
-    */
-    Matrix<int>* n = new Matrix<int>();
-    n->print();
-    n->insert(2,1,0);
-    n->insert(5,0,0);
-    n->insert(3,0,1);
-    n->insert(1,1,1);
-    n->print();
-
-    n->expand_matrix(2);
-    n->print();
-
+    
+    clear();
+    menu();
     return 0;
+}
+
+void menu() {
+
+    short menuIndex = 0;
+    short choice    = 0;
+
+    do {
+        printMenu(menu_text[menuIndex]);
+        std::cout << "\n" << "->";
+        std::cin >> choice;
+        clear();
+
+    } while(choice != 0);
+}
+
+void printMenu(std::list<std::string> texts) {
+    std::list <std::string> :: iterator text; 
+    for(text = texts.begin(); text != texts.end(); ++text) 
+        std::cout << '\n' << *text; 
+    std::cout << '\n'; 
 }
